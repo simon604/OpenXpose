b73e9da503336b7c75f3d1bd72e47ed3b016ea1c
Simon Wong <simonwong85@gmail.com>
2012-03-04 21:36:38 +1100

    Setup unit testing placeholder

---
+++ app/protected/tests/unit/CommonTest.php
@@ -0,0 +1,21 @@
+<?php
+class CommentTest extends CDbTestCase
+{
+
+	public function testHelloWorld()
+	{
+		$str = 'Hello World';
+
+		$this->assertEquals('Hello World', $str);
+	}
+
+	public function testHelloWorld2()
+	{
+		$str = 'Hello World2';
+
+		$this->assertEquals('Hello World', $str);
+	}
+ 
+}
+
+

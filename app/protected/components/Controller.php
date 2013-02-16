085a3095fbca6a0c76430f503c4a405ba5b34140
Simon Wong <simonwong85@gmail.com>
2013-02-16 12:05:21 +0800

    Removing Git testing code

--- app/protected/components/Controller.php
+++ app/protected/components/Controller.php
@@ -20,14 +20,4 @@ class Controller extends CController
 	 * for more details on how to specify this property.
 	 */
 	public $breadcrumbs=array();
-
-	/**
-	 * [test description]
-	 * @param  [type] $test [description]
-	 * @return [type]       [description]
-	 */
-	public function test($test)
-	{
-		echo 'Testing git!';
-	}
 }
